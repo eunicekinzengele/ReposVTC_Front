8f53bdb512d835e1de1f3c7bd0c6843d43852c1b
eunicekinzengele <eunicekinzengele0@gmail.com>
2023-07-31 18:07:57 +0200

    creation of a banner

---
+++ pages/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div className=" border-2 ">
+      <div className=" bg-[url('../Images/bannerImg.jpg')] bg-no-repeat bg-center bg-cover object-contain relative w-full h-52">
+        <div className="  absolute bottom-0 right-0 left-0 w-full h-full bg-black/40 pt-24  flex justify-center text-white">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/RetailsCarPage.jsx
+++ pages/RetailsCarPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Topbar from '../components/layout/Topbar'
+import Header from './Header'
 
 export default function RetailsCarPage() {
   return (
     <div>
         <Topbar/>
+        <Header/>
+        
     </div>
   )
 }
