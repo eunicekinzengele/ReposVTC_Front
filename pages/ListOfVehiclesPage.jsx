40b89bee81b1a3fccce03ccba36bac4fe821fd34
eunicekinzengele <eunicekinzengele0@gmail.com>
2023-08-03 21:29:30 +0200

    creation of a list of vehicle page

--- pages/ListOfVehiclesPage.jsx
+++ pages/ListOfVehiclesPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Topbar from '../components/layout/Topbar'
+import AllVehicles from './AllVehicles'
+import Banner from './Banner'
+import Header from './Header'
 
 export default function ListOfVehiclesPage() {
   return (
-    <div>ListOfVehiclesPage</div>
+    <div>
+        <Topbar/>
+        <Header/>
+        <Banner/>
+        <AllVehicles/>
+    </div>
   )
 }
